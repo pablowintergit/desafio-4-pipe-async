041d45c8bab3c43d8c412a4e3d56e8b26225ddcc
pablowinter <pablo.winter@gmail.com>
2024-04-17 00:01:20 -0300

    Implementacion de Promesas con Pipe Async

--- src/app/async-promise-pipe/async-promise-pipe.component.ts
+++ src/app/async-promise-pipe/async-promise-pipe.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
+import { Product } from '../model/product.model';
 
 @Component({
   selector: 'app-async-promise-pipe',
   templateUrl: './async-promise-pipe.component.html',
   styleUrl: './async-promise-pipe.component.scss'
 })
-export class AsyncPromisePipeComponent {
+export class AsyncPromisePipeComponent implements OnInit {
 
+  products$?: Promise<Product[]>;
+
+  constructor(private productsService: ProductsService) { 
+
+  }
+  ngOnInit(): void {
+    this.products$=new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.productsService.getProducts());
+      }, 3000);
+    });
+  }
 }
